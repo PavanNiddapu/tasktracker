39ebaa21ef17c90c7435cb535f855d97de9a1879
lalith.niddapu <lalith.niddapu@thoughtworks.com>
2022-04-21 22:36:51 +0530

    Add Proxy to backend

--- UI/frontend/src/App.js
+++ UI/frontend/src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = useState("Hello from React")
+    const [json, setJson] = useState("Hello from React")
+
+    const onFirstLiClickHandler = () => {
+        fetchText()
+    }
+
+    const onSecondsLiClickHandler = () => {
+        fetchJson()
+    }
+
+    let fetchText = async () => {
+        const response = await fetch('/api/demo');
+        await response.text().then(data => setText(data)).catch(reason => setText("Error"))
+    }
+
+    let fetchJson = async () => {
+        const response = await fetch('/api/json/hello');
+        await response.text().then(data => setJson(data)).catch(reason => setJson("Error"))
+    }
+
+    return (
+        <div className="App">
+            <header>
+                <img src={logo} className="App-logo" alt="logo"/>
+            </header>
+            <ul>
+                <li onClick={onFirstLiClickHandler}>
+                    {text}
+                </li>
+                <li onClick={onSecondsLiClickHandler}>
+                    {json}
+                </li>
+            </ul>
+
+        </div>
+    );
 }
 
 export default App;
