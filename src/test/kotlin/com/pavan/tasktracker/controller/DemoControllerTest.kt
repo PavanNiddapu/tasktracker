fb22740e2b767a6246984f93c4e321e26d8bd9ec
lalith.niddapu <lalith.niddapu@thoughtworks.com>
2022-04-30 11:14:40 +0530

    Lalith | Move the /tasks API to TaskController.kt

--- src/test/kotlin/com/pavan/tasktracker/controller/DemoControllerTest.kt
+++ src/test/kotlin/com/pavan/tasktracker/controller/DemoControllerTest.kt
@@ -1,8 +1,5 @@
 package com.pavan.tasktracker.controller
 
-import com.pavan.tasktracker.model.Task
-import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.test.web.reactive.server.WebTestClient
@@ -12,16 +9,4 @@ class DemoControllerTest(
     @Autowired val webTestClient: WebTestClient
 ) {
 
-    @Test
-    fun `should return count of tasks`() {
-        val response = webTestClient.get()
-            .uri("/api/tasks")
-            .exchange()
-            .expectStatus().isOk
-            .expectBodyList(Task::class.java)
-            .returnResult()
-            .responseBody!!
-
-        response.size shouldBe 2
-    }
 }
