6d01b5aafe71b7c62766acfc4d2f6c916279a478
lalith.niddapu <lalith.niddapu@thoughtworks.com>
2022-04-30 11:10:40 +0530

    Lalith | Moving the /tasks API to TaskController.kt

--- src/main/kotlin/com/pavan/tasktracker/controller/TaskController.kt
+++ src/main/kotlin/com/pavan/tasktracker/controller/TaskController.kt
@@ -1,21 +1,18 @@
 package com.pavan.tasktracker.controller
 
+import com.pavan.tasktracker.model.Task
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
 
 @RestController
 @RequestMapping("/api")
-class DemoController {
+class TaskController {
 
-    @GetMapping("/demo")
-    fun getSample(): String {
-        return "Hello Pavan"
-    }
-
-    @GetMapping("/json/hello")
-    fun getSampleJson(): Map<String, String> {
-        return mapOf("name" to "Lalith")
+    @GetMapping("/tasks")
+    fun getTasks(): Flux<Task> {
+        return Flux.just(Task(), Task())
     }
 
 }
