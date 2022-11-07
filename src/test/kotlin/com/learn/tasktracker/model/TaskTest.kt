c9b5bacf0ec9254c232081f141a436b7741641aa
Lalith Niddapu <lalith.niddapu@thoughtworks.com>
2022-11-07 14:53:42 +0530

    Lalith | Refactor task test

--- src/test/kotlin/com/learn/tasktracker/model/TaskTest.kt
+++ src/test/kotlin/com/learn/tasktracker/model/TaskTest.kt
@@ -12,22 +12,14 @@ class TaskTest {
     fun `task should have created date`() {
         val someFixedDts = LocalDateTime.of(2022, 11, 11, 11, 11, 11)
         withConstantNow(someFixedDts) {
-            val task = Task(
-                id = 1,
-                name = "SomeTask",
-                description = "Description"
-            )
+            val task = sampleTask()
             task.createdDate shouldBe someFixedDts
         }
     }
 
     @Test
     fun `task should have list of TimePiece`() {
-        val task = Task(
-            id = 1,
-            name = "task1",
-            description = "some desc"
-        )
+        val task = sampleTask()
         task.startActivity()
         task.stopActivity()
         task.startActivity()
@@ -37,28 +29,27 @@ class TaskTest {
 
     @Test
     fun `should throw unexpected state exception when starting a already started TimePiece`() {
-        val task = Task(
-            id = 1,
-            name = "task1",
-            description = "some desc"
-        )
-        task.startActivity();
+        val task = sampleTask()
+        task.startActivity()
         assertThrows<UnExpectedState> {
-            task.startActivity();
+            task.startActivity()
         }
     }
 
     @Test
     fun `should throw unexpected state exception when stopping a already stopped TimePiece`() {
-        val task = Task(
-            id = 1,
-            name = "task1",
-            description = "some desc"
-        )
-        task.startActivity();
-        task.stopActivity();
+        val task = sampleTask()
+        task.startActivity()
+        task.stopActivity()
         assertThrows<UnExpectedState> {
-            task.stopActivity();
+            task.stopActivity()
         }
     }
+
+    private fun sampleTask() = Task(
+        id = 1,
+        name = "task1",
+        description = "some desc"
+    )
+
 }
