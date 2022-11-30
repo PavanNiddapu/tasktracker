003fbceac0507954ae6a2fe6911d9796def78b1d
Lalith Niddapu <lalith.niddapu@thoughtworks.com>
2022-11-30 10:21:42 +0530

    Lalith | Add task start and stop actions

--- src/main/kotlin/com/learn/tasktracker/controller/TaskController.kt
+++ src/main/kotlin/com/learn/tasktracker/controller/TaskController.kt
@@ -5,10 +5,7 @@ import com.learn.tasktracker.service.TaskService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
@@ -24,4 +21,16 @@ class TaskController(
     fun createTask(task: Task): ResponseEntity<Task> {
         return ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTask(task))
     }
+
+    @GetMapping("/task/start/{id}")
+    fun startTask(@PathVariable id: Int): TaskActionResponse {
+        return if (taskService.startTask(id)) TaskActionResponse("success") else TaskActionResponse("failed")
+    }
+
+    @GetMapping("/task/stop/{id}")
+    fun stopTask(@PathVariable id: Int): TaskActionResponse {
+        return if (taskService.stopTask(id)) TaskActionResponse("success") else TaskActionResponse("failed")
+    }
+
+    data class TaskActionResponse(val message: String)
 }
